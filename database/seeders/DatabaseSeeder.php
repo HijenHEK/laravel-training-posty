350a0ab2c8bf0d75333fbcec2f954fd52afee9f5
HijenHek <k.hijen@gmail.com>
2020-12-01 11:02:05 +0100

    factories + seeds + post table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            "name" => "admin",
+            "username" => "admin",
+            "email" => "admin@posty.com",
+            "password" => Hash::make("password")
+        ]);
+        User::factory(10)->create();
+        Post::factory(30)->create();
     }
 }

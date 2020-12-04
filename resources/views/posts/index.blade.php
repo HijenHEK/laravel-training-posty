10f538ba778ef6b7a1103d1b450ddaab0e49da6a
HijenHek <k.hijen@gmail.com>
2020-12-04 12:59:44 +0100

    showing post + single comment

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,15 +33,32 @@
 
         @foreach ($posts as $post)
         <div class="bg-white w-full border-2 rounded-lg py-2 px-4 my-2">
-            <a href="{{route('profile' , $post->user->username)}}" class="mb-2 font-bold">
-                {{$post->user->name}}
-            </a>
-            <span class="text-xs text-gray-500"> {{ $post->created_at->diffForHumans()}}</span>
-            <p>
-                {{$post->body}}
+            <div class="flex justify-between ">
+                <div class="user">
+                    <a href="{{route('profile' , $post->user->username)}}" class="mb-2 font-bold">
+                        {{$post->user->name}}
+                    </a>
+                    <span class="text-xs text-gray-500"> {{ $post->created_at->diffForHumans()}}</span>
+                </div>
+                @can('delete' , $post)
+                <form action="{{route('posts.delete' , $post->id)}}" method="post" class="inline">
+                    @method("DELETE")
+                    @csrf
+                    <button type="submit" class="text-xs text-white bg-red-600 border-2 px-1 rounded-lg">
+                        delete
+                    </button>
+                </form>
+            @endcan
+            </div>
+            
+            <a  href="{{route('posts.show' , $post->id)}}" >
 
-            </p>
-            <div class="flex justify-between px-2">
+                <div>
+                    {{$post->body}}
+                </div>
+
+            </a>
+            <div class="flex px-2">
                 <div>
                     @auth
                     <form action="{{route('like',$post->id)}}" method="post" class="inline">
@@ -61,17 +78,16 @@
                         class=" font-bold text-xs @auth  @if (Auth::user()->like($post)) text-blue-700 @endif @endauth">
                         {{$post->likes->count() .' '. Str::plural("like", $post->likes->count()) }}
                     </span>
+                    <a href="{{route('posts.show' , $post->id)}}" >
 
-                </div>
-                @can('delete' , $post)
-                <form action="{{route('posts.delete' , $post->id)}}" method="post" class="inline">
-                    @method("DELETE")
-                    @csrf
-                    <button type="submit" class="text-xs text-white bg-red-600 border-2 px-1 rounded-lg">
-                        delete
-                    </button>
-                </form>
-                @endcan
+                        <span
+                        class=" mx-3 px-3 font-bold text-xs">
+                        {{$post->comments->count() .' '. Str::plural("comment", $post->comments->count()) }}
+                        </span>
+
+                    </a>
+                    
+            </div>
             </div>
 
         </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,14 @@
 @extends('layout.app')
 
 @section('content')
-<div class="w-8/12 mx-auto bg-white p-5 my-5 h-full">
+<div class="w-8/12 mx-auto p-5 my-5 h-full">
 
 <div class="bg-white w-full  py-2 px-4 my-2">
-    <a href="{{route('profile' , $post->user->username)}}" class="mb-2 font-bold">
+    <a href="{{route('profile' , $post->user->username)}}" class="mb-2 text-lg font-bold">
         {{$post->user->name}}
     </a>
-    <span class="text-xs text-gray-500"> {{ $post->created_at->diffForHumans()}}</span>
-    <p>
+    <span class="text-sm text-gray-500"> {{ $post->created_at->diffForHumans()}}</span>
+    <p class=>
         {{$post->body}}
 
     </p>
@@ -22,14 +22,14 @@
                 @method("DELETE")
                 @endif
 
-                <button type="submit" class="text-xs text-gray-700 border-2 px-1 rounded-lg">
+                <button type="submit" class="text-sm text-gray-700 border-2 px-1 rounded-lg">
                     {{Auth::user()->like($post) ? 'Unlike' : 'Like'}}
                 </button>
             </form>
             @endauth
 
             <span
-                class=" font-bold text-xs @auth  @if (Auth::user()->like($post)) text-blue-700 @endif @endauth">
+                class=" font-bold text-sm @auth  @if (Auth::user()->like($post)) text-blue-700 @endif @endauth">
                 {{$post->likes->count() .' '. Str::plural("like", $post->likes->count()) }}
             </span>
 
@@ -38,7 +38,7 @@
         <form action="{{route('posts.delete' , $post->id)}}" method="post" class="inline">
             @method("DELETE")
             @csrf
-            <button type="submit" class="text-xs text-white bg-red-600 border-2 px-1 rounded-lg">
+            <button type="submit" class="text-sm text-white bg-red-600 border-2 px-1 rounded-lg">
                 delete
             </button>
         </form>
@@ -46,6 +46,24 @@
     </div>
 
 </div>
+
+    @foreach ($post->comments as $comment)
+        
+        <div class="bg-white w-full  my-2 p-3 mx-5">
+
+            <a href="{{route('profile' , $comment->user->username)}}" class="mb-2 text-sm font-bold">
+                {{$comment->user->name}}
+            </a>
+            <span class="text-xs text-gray-500"> {{ $comment->created_at->diffForHumans()}}</span>
+            <p class="text-sm">
+                {{$comment->content}}
+
+            </p>
+
+        </div>
+
+
+    @endforeach
 
 </div>
 

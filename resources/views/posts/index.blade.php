210e59be9b1d7dbd8cc379aecb82982bfbcd98f4
HijenHek <k.hijen@gmail.com>
2020-12-04 13:56:02 +0100

    lg:w

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-<div class="w-8/12 mx-auto bg-white p-5 my-5 h-full">
+<div class="lg:w-8/12 w-full mx-auto bg-white p-5 my-5 h-full">
     @auth
     <form action="{{route('posts')}}" method="post">
         @csrf

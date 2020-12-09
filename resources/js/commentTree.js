d4e63a86bcb914cef813f5d8eb6cf1eafaa6933f
HijenHek <k.hijen@gmail.com>
2020-12-09 07:03:25 +0100

    laravel/websoket

---
+++ resources/js/commentTree.js
@@ -0,0 +1,20 @@
+export default {
+  label: 'comment',
+  nodes: [
+    {
+      label: 'reply1',
+      nodes: [
+        {
+          label: 'reply1.1',
+        
+          nodes: [
+            {
+              label: 'reply.1.1',
+            }
+          ]
+        }
+      ]
+    }, 
+   
+  ]
+}
